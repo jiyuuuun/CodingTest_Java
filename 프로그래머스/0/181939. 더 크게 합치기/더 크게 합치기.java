2ccb55c5020931045b63690b4bc7cfef42d9dc19
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-01-19 19:39:38 +0900

    [level 0] Title: 더 크게 합치기, Time: 9.90 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,9 +1,7 @@
 class Solution {
     public int solution(int a, int b) {
-        String result1 = String.valueOf(a) + String.valueOf(b);
-        int answer1 = Integer.parseInt(result1);
-        String result2 = String.valueOf(b) + String.valueOf(a);
-        int answer2 = Integer.parseInt(result2);
+        int answer1 = Integer.parseInt(a+""+b);
+        int answer2 = Integer.parseInt(b+""+a);
         
         if (answer1 >= answer2) {
             return answer1;
