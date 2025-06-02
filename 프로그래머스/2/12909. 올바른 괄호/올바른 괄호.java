8e7c080b56c469e389cbcee84fc892372161338d
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-06-02 20:55:50 +0900

    [level 2] Title: 올바른 괄호, Time: 17.20 ms, Memory: 54.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack();
+        
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '(') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                if (stack.isEmpty()) {
+                    answer = false;
+                    return answer;
+                }
+                stack.pop();
+            }
+        }
+        
+        if(stack.isEmpty()) {
+            answer = true;
+        } else {
+            answer = false;
+        }
+        
+        return answer;
+    }
+}
