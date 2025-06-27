912ccafd86c95d1a0419c76f7998716b21adbecd
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-06-27 23:00:43 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1236 ms, Memory: 180608 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[][] points = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            points[i][0] = sc.nextInt();
+            points[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(points, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] != o2[0]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(points[i][0]).append(" ").append(points[i][1]).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
