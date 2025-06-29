99c30cc8091f04038d110a41219d4cc4b3da8936
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-06-29 21:38:57 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] A;
+    static boolean[] check;
+    static int count = 0;
+    static int N,M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        A = new int[N+1][N+1];
+        check = new boolean[N+1];
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            A[a][b] = A[b][a] = 1;
+        }
+        DFS(1);
+        System.out.print(count-1);
+    }
+    public static void DFS(int V) {
+        check[V] = true;
+        count++;
+        for (int i = 1; i <= N; i++) {
+            if (A[V][i] == 1 && !check[i]) {
+                DFS(i);
+            }
+        }
+    }
+}
