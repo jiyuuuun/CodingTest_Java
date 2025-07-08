f3f10bf47faf316947a310236a525f694e79e993
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-07-08 14:32:00 +0900

    [level 2] Title: 괄호 회전하기, Time: 30.79 ms, Memory: 97.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        for(int i=0; i<s.length(); i++) {
+            String rotated = s.substring(i) + s.substring(0,i);
+            if (isValid(rotated)) answer++;
+        }
+        
+        return answer;
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c:s.toCharArray()) {
+            if (c=='(' || c=='[' || c=='{') {
+                stack.push(c);
+            } else {
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if(c==')' && top != '(' ||
+                   c==']' && top != '[' ||
+                   c=='}' && top != '{') {
+                    return false;  
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
