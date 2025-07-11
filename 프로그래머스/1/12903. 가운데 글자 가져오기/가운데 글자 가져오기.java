4365410fe6a672e6995916328d38c150bd16ecca
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-07-11 20:18:30 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 92.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String s) {
+        int len = s.length();
+        int mid = len / 2;
+        
+        return (len % 2 == 0) ? s.substring(mid-1, mid+1) : s.substring(mid, mid+1);
+    }
+}
