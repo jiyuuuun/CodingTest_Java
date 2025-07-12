c78e3ca1b6ff487ad74f3f85aa98257ecc79697b
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-07-12 23:13:06 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.72 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i=0; i<n/2; i++) {
+            sb.append("수박");
+        }
+        
+        if(n % 2 != 0) {
+            sb.append("수");
+        }
+        
+        answer = sb.toString();
+        
+        return answer;
+    }
+}
