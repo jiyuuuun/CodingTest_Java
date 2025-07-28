45bedb5ef2e3f8e3f21cb7b7212719c697f8f7f8
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-07-28 21:26:04 +0900

    [level 1] Title: K번째수, Time: 0.52 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int len = commands.length;
+        int[] answer = new int[len];
+        
+        for(int i=0; i<len; i++) {
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+        
+        return answer;
+    }
+}
