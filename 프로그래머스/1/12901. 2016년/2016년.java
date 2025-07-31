97e2afe25e4ff5efe0bec6418b490d384d137a19
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-08-01 00:59:38 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        int[] days = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] week = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int totalDays = 0;
+        
+        for(int i=1; i<a; i++) {
+            totalDays += days[i];
+        }
+        
+        totalDays = totalDays + b - 1;
+        
+        answer = week[totalDays%7];
+        
+        return answer;
+    }
+}
