d29a423645f7a4ba01475e280a91282e24aca3bb
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-09-14 20:44:24 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14736 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,51 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int[] dx = {0,1,0,-1};
-    static int[] dy = {1,0,-1,0};
+    static int N,M;
+    static int[] dx = {-1,1,0,0}; // 상하좌우
+    static int[] dy = {0,0,-1,1};
     static boolean[][] visited;
-    static int[][] A;
-    static int N, M;
+    static int[][] maze;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        A = new int[N][M];
+
+        maze = new int[N][M];
         visited = new boolean[N][M];
+
+        // 미로 입력
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String line = st.nextToken();
+            String line = br.readLine();
             for (int j = 0; j < M; j++) {
-                A[i][j] = Integer.parseInt(line.substring(j,j+1));
+                maze[i][j] = line.charAt(j) - '0';
             }
         }
-        BFS(0,0);
-        System.out.println(A[N-1][M-1]);
+
+        System.out.print(bfs(0,0));
     }
 
-    private static void BFS(int i, int j) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {i,j});
-        visited[i][j] = true;
-        while(!queue.isEmpty()) {
-            int now[] = queue.poll();
-            for (int k = 0; k < 4; k++) {
-                int x = now[0] + dx[k];
-                int y = now[1] + dy[k];
-                if(x>=0 && y>=0 && x<N && y<M) {
-                    if(A[x][y]!=0 && !visited[x][y]) {
-                        visited[x][y] = true;
-                        A[x][y] = A[now[0]][now[1]] + 1;
-                        queue.add(new int[] {x, y});
+    static int bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x,y});
+        visited[x][y] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+
+            // 상하좌우 탐색
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                // 범위 체크 + 길 체크 + 방문 체크
+                if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                    if (maze[nx][ny] == 1 && !visited[nx][ny]) {
+                        visited[nx][ny] = true;
+                        maze[nx][ny] = maze[cx][cy] +1;
+                        q.offer(new int[]{nx,ny});
                     }
                 }
             }
         }
+        return maze[N-1][M-1]; // 도착지점 값 = 최단거리
     }
 }
