d12e843d387d6cd26a4bbcacb0127b4bce6fd29a
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-09-16 00:37:55 +0900

    [Silver II] Title: 유기농 배추, Time: 132 ms, Memory: 15844 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[] dx = {-1,1,0,0}; // 상하좌우
+    static int[] dy = {0,0,-1,1};
+
+    static int[][] map;
+    static boolean[][] visited;
+
+    static int M; // 가로
+    static int N; // 세로
+    static int K; // 배추 위치 개수
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스의 개수
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            map = new int[N][M];
+            visited = new boolean[N][M];
+
+            // 배추밭 입력
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                map[y][x] = 1;
+            }
+
+            int count = 0;
+
+            // 전체 밭 탐색
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (map[i][j] == 1 && !visited[i][j]) {
+                        dfs(i,j);
+                        count++;
+                    }
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
+
+    static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx>=0 && ny>=0 && nx<N && ny<M) {
+                if (map[nx][ny]==1 && !visited[nx][ny]) {
+                    dfs(nx,ny);
+                }
+            }
+        }
+    }
+}
