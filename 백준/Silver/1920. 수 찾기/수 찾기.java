e856f0bc368b21c2ff06be1c1261523fb4e80509
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-09-17 21:06:49 +0900

    [Silver IV] Title: 수 찾기, Time: 1140 ms, Memory: 62120 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,23 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int N = sc.nextInt();
-        Set<Integer> set = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
+        int[] n = new int[N];
+
+        String[] input1 = br.readLine().split(" ");
         for (int i = 0; i < N; i++) {
-            set.add(sc.nextInt());
+            n[i] = Integer.parseInt(input1[i]);
+        }
+        Arrays.sort(n);
+
+        int M = Integer.parseInt(br.readLine());
+        int[] m = new int[M];
+
+        String[] input2 = br.readLine().split(" ");
+        for (int i = 0; i < M; i++) {
+            m[i] = Integer.parseInt(input2[i]);
         }
 
-        int M = sc.nextInt();
+        // 이분 탐색
         for (int i = 0; i < M; i++) {
-            if(set.contains(sc.nextInt())) {
-                System.out.println(1);
-            } else {
+            int left = 0;
+            int right = N-1;
+            int find = m[i];
+
+            while (left <= right) {
+                int mid = left + (right-left) / 2;
+
+                if (n[mid] == find) {
+                    System.out.println(1);
+                    break;
+                } else if (n[mid] > find) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            if (left > right) {
                 System.out.println(0);
             }
         }
+
     }
 }
