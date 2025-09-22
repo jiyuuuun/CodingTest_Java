029d204312b59eebd6f52344d62b0c163795fb3d
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-09-23 00:27:28 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 580 ms, Memory: 139288 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine()); // 수열 크기
+        String[] input = br.readLine().split(" ");
+
+        int[] A = new int[a];
+        for (int i = 0; i < a; i++) {
+            A[i] = Integer.parseInt(input[i]);
+        }
+
+        ArrayList<Integer> lis = new ArrayList<>();
+
+        for (int num : A) {
+            if (lis.isEmpty() || lis.get(lis.size()-1) < num) {
+                lis.add(num); // 마지막 값도가 크면 추가
+            } else {
+                // 이분 탐색으로 교체 위치 찾기
+                int left = 0;
+                int right = lis.size()-1;
+                while (left < right) {
+                    int mid = (left + right) / 2;
+                    if (lis.get(mid) < num) {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
+                }
+                lis.set(right, num);
+            }
+        }
+
+        System.out.print(lis.size());
+    }
+}
