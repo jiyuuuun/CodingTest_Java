610520faf40113781508f693b036e3edc9d9be05
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-09-30 22:03:50 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,25 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static int answer = 0;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String example = sc.nextLine();
-        String[] str = example.split("-");
-        for (int i = 0; i < str.length; i++) {
-            int temp = mySum(str[i]);
-            if(i==0) answer = answer + temp;
-            else answer = answer - temp;
-        }
-        System.out.println(answer);
-    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    private static int mySum(String a) {
-        int sum = 0;
-        String[] temp = a.split("[+]");
-        for (int i = 0; i < temp.length; i++) {
-            sum = sum + Integer.parseInt(temp[i]);
+        String[] minusSplit = br.readLine().split("-");
+        int answer = 0;
+
+        for (String part : minusSplit[0].split("\\+")) {
+            answer += Integer.parseInt(part);
         }
-        return sum;
+
+        for (int i = 1; i < minusSplit.length; i++) {
+            String[] plusSplit = minusSplit[i].split("\\+");
+            int sum = 0;
+            for (String part : plusSplit) {
+                sum += Integer.parseInt(part);
+            }
+            answer -= sum;
+        }
+
+        System.out.print(answer);
+
     }
 }
