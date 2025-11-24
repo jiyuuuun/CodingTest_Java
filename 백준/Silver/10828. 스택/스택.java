80529524414fa66b3d56609cbafcd38793981cb8
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-11-25 00:22:18 +0900

    [Silver IV] Title: 스택, Time: 292 ms, Memory: 19480 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,37 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 명령 수
+
         Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < num; i++) {
-            String str = sc.next();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
 
-            if(str.equals("push")) {
-                int pushNum = sc.nextInt();
-                stack.push(pushNum);
-            } else if (str.equals("pop")){
-                if(stack.isEmpty()) {
-                    System.out.println(-1);
-                } else {
-                    System.out.println(stack.pop());
-                }
-            } else if (str.equals("size")) {
-                System.out.println(stack.size());
-            } else if(str.equals("empty")) {
-                if(stack.isEmpty()) {
-                    System.out.println(1);
-                } else {
-                    System.out.println(0);
-                }
-            } else {
-                if(stack.isEmpty()) {
-                    System.out.println(-1);
-                } else {
-                    System.out.println(stack.peek());
-                }
+            switch (str) {
+                case "push":
+                    int p = Integer.parseInt(st.nextToken());
+                    stack.push(p);
+                    break;
+                case "pop":
+                    if (!stack.isEmpty()) {
+                        System.out.println(stack.pop());
+                    } else {
+                        System.out.println(-1);
+                    }
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    if (stack.isEmpty()) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                    break;
+                case "top":
+                    if (!stack.isEmpty()) {
+                        System.out.println(stack.peek());
+                    } else {
+                        System.out.println(-1);
+                    }
+                    break;
             }
         }
     }
