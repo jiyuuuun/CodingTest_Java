8f4655880f877de7672f0e413aa5d2fc438c00f8
Jun <132816893+jiyuuuun@users.noreply.github.com>
2025-12-04 17:49:52 +0900

    [Gold V] Title: AC, Time: 668 ms, Memory: 98484 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,53 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int T = sc.nextInt(); // 테스트 케이스 개수
+        int T = Integer.parseInt(br.readLine());
 
-        while(T-- > 0) {
-            String p = sc.next(); // 수행할 함수
-            int n = sc.nextInt(); // 배열에 들어있는 개수
-            String input = sc.next();
+        while (T-- > 0) {
+            String p = br.readLine(); // 수행할 함수
+            int n = Integer.parseInt(br.readLine()); // 배열에 들어있는 개수
+            String arr = br.readLine();
 
-            input = input.replace("[", "").replace("]", "");
-            String[] parts = input.isEmpty() ? new String[0] : input.split(",");
+            Deque<Integer> dq = new ArrayDeque<>();
 
-            Deque<Integer> arr = new ArrayDeque<>();
-            for (int i = 0; i < n; i++) {
-                arr.add(Integer.parseInt(parts[i]));
+            // 배열 파싱
+            if (n > 0) {
+                arr = arr.substring(1, arr.length()-1);
+                String[] nums = arr.split(",");
+                for (String num : nums) {
+                    dq.add(Integer.parseInt(num));
+                }
             }
 
-            boolean reversed = false;
+            boolean reverse = false;
             boolean error = false;
 
-            for (int i = 0; i < p.length(); i++) {
-                char ch = p.charAt(i);
-
-                if (ch == 'R') {
-                    reversed = !reversed;
+            for (char cmd : p.toCharArray()) {
+                if (cmd == 'R') {
+                    reverse = !reverse;
                 } else { // D
-                    if (arr.isEmpty()) {
-                        System.out.println("error");
+                    if (dq.isEmpty()) {
                         error = true;
                         break;
                     }
-                    if (!reversed) arr.pollFirst(); // 정방향 -> 앞에서 제거
-                    else arr.pollLast(); // 뒤집힌 상태 -> 뒤에서 제거
+                    if (!reverse) {
+                        dq.pollFirst();
+                    } else {
+                        dq.pollLast();
+                    }
                 }
             }
 
-            if(!error) {
-                StringBuilder sb = new StringBuilder();
-                sb.append("[");
-                while(!arr.isEmpty()) {
-                    sb.append(reversed? arr.pollLast() : arr.pollFirst());
-                    if(!arr.isEmpty()) sb.append(",");
-                }
-                sb.append("]");
-                System.out.println(sb.toString());
+            if (error) {
+                sb.append("error\n");
+                continue;
             }
+
+            // 출력
+            sb.append("[");
+            if (!dq.isEmpty()) {
+                if (!reverse) {
+                    while (dq.size()>1) sb.append(dq.pollFirst()).append(",");
+                    sb.append(dq.pollFirst());
+                } else {
+                    while (dq.size()>1) sb.append(dq.pollLast()).append(",");
+                    sb.append(dq.pollLast());
+                }
+            }
+            sb.append("]\n");
         }
+        
+        System.out.print(sb);
     }
 }
