b80004b88e67ae570c136ad0718819165599da39
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-01-05 00:18:17 +0900

    [Silver III] Title: 회전하는 큐, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,44 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = sc.nextInt(); // 큐의 크기
-        int M = sc.nextInt(); //뽑아내려고 하는 수의 개수
-        int answer = 0; // 연산 횟수 누적
+        int N = Integer.parseInt(st.nextToken()); // 큐의 크기
+        int M = Integer.parseInt(st.nextToken()); // 뽑아내려고 하느 수 개수
+        int answer = 0;
 
-        Deque<Integer> dq = new ArrayDeque<>();
+        Deque<Integer> dq = new LinkedList<>();
         for (int i = 1; i <= N; i++) {
             dq.add(i);
         }
 
-        for (int i = 0; i < M; i++) {
-            int target = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
 
-            int index = 0;
-            for (int num : dq) {
-                if(num == target) break;
-                index++;
+        while (M-- > 0) {
+            int target = Integer.parseInt(st.nextToken());
+
+            // 현재 위치 찾기
+            int idx = 0;
+            for (int x : dq) {
+                if (target == x) break;
+                idx++;
             }
 
-            int left = index;
-            int right = dq.size() - index;
-
-            if(left <= right) {
-                for (int j = 0; j < left; j++) {
+            // 회전 방향 결정
+            if (idx <= dq.size() / 2) {
+                // 왼쪽 회전
+                while (dq.peekFirst() != target) {
                     dq.addLast(dq.pollFirst());
                     answer++;
                 }
             } else {
-                for (int j = 0; j < right; j++) {
+                // 오른쪽 회전
+                while (dq.peekFirst() != target) {
                     dq.addFirst(dq.pollLast());
                     answer++;
                 }
             }
 
+            // 제거
             dq.pollFirst();
         }
+
         System.out.print(answer);
+
     }
 }
