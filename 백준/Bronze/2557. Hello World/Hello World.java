df8e282cb117d4be97b911576f9caa1795906216
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-01-05 00:20:46 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.print("Hello World!");
+    }
+}
