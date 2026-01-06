26a223e0268b288eef025f21f25fccb7b5ca5dd9
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-01-06 21:24:34 +0900

    [Bronze V] Title: 고양이, Time: 92 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+
+// """ 텍스트 블록으로 보이는 그대로 출력 가능 (Java 15 이상 지원)
+// \ 하나는 이스케이프 시작 문자라서 \ 출력하려면 \\ 필요
