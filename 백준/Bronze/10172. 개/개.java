211ee7ce1a8e04a7ff2c6d67f89eb27bc68ffd36
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-01-06 21:30:02 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
+
+// \ 하나는 이스케이프 시작 문자라서 \ 출력하려면 \\ 필요
+// " 출력하려면 \" 필요
