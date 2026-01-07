5ee2f682f75f4b036e6b50c4231a8a3204fd8517
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-01-07 18:42:15 +0900

    [Bronze V] Title: 시험 성적, Time: 100 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int score = Integer.parseInt(br.readLine());
+        
+        if (90<=score && score<=100) {
+            System.out.print("A");
+        } else if (80<=score && score<90) {
+            System.out.print("B");
+        } else if (70<=score && score<80) {
+            System.out.print("C");
+        } else if (60<=score && score<70) {
+            System.out.print("D");
+        } else {
+            System.out.print("F");
+        }
+    }
+}
