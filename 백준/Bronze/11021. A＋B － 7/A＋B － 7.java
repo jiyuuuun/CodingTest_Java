756fc2a68fdee1dfb2102668afc78cd684e843c8
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-01-15 00:26:26 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 16088 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int i=1; i<=T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int sum = A+B;
+            
+            System.out.println("Case #" + i + ": " + sum);
+        }
+    }
+}
