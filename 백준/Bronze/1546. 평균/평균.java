4fab22184ebdc651a22b5339a74c7da62c4d8a84
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-01-15 18:27:21 +0900

    [Bronze I] Title: 평균, Time: 116 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,18 +1,25 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int A[] = new int[n];
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 과목 개수
+        
         long sum = 0;
         long max = 0;
-        for (int i = 0; i < n; i++) {
-            A[i] = sc.nextInt();
-            if (A[i] > max) max=A[i];
-            sum += A[i];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for (int i=0; i<N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            
+            if (num > max) max = num;
+            sum += num;
         }
-        System.out.println(sum*100.0/n/max);
+        
+        System.out.println(sum*100.0/max/N);
+        
     }
 }
