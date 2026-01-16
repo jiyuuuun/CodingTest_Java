3112595415ea2c0a7220cadda5a809dd4d9f0d0a
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-01-16 22:39:26 +0900

    [Bronze V] Title: 문자열, Time: 108 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        while(T-- > 0) {
+            String S = br.readLine();
+            int len = S.length();
+            
+            StringBuilder sb = new StringBuilder();
+            sb.append(S.charAt(0)).append(S.charAt(len-1));
+            
+            System.out.println(sb);
+        }
+    }
+}
