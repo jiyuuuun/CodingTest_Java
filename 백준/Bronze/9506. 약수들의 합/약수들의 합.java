ce5e9a853e11e7d764272fd8ddd6fe1ef1a2e6c9
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-01-27 20:22:00 +0900

    [Bronze I] Title: 약수들의 합, Time: 128 ms, Memory: 15072 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -1,30 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder answer = new StringBuilder();
-
-        int n;
-        while ((n = Integer.parseInt(br.readLine())) != -1) {
+        StringBuilder sb = new StringBuilder();
+        
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == -1) break;
+            
             int sum = 0;
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = 1; i < n; i++) {
+            List<Integer> list = new ArrayList<>();
+            
+            for (int i=1; i<n; i++) {
                 if (n % i == 0) {
                     sum += i;
-                    if (sb.length() > 0) sb.append(" + ");
-                    sb.append(i);
+                    list.add(i);
                 }
             }
-
-            if (n == sum) answer.append(n + " = " + sb).append("\n");
-            else answer.append(n + " is NOT perfect.").append("\n");
+            
+            if (n == sum) {
+                sb.append(n + " = ");
+                for (int i=0; i<list.size(); i++) {
+                    sb.append(list.get(i));
+                    
+                    if (i != list.size() - 1) {
+                        sb.append(" + ");
+                    }
+                }
+            } else {
+                sb.append(n + " is NOT perfect.");
+            }
+            
+            sb.append("\n");
         }
-
-        System.out.print(answer);
+        
+        System.out.print(sb);
     }
 }
