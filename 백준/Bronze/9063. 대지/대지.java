1dfe1f649c9c21671fcd7376afc90b003da5046e
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-01-28 23:19:18 +0900

    [Bronze III] Title: 대지, Time: 272 ms, Memory: 29128 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] arrX = new int[N];
+        int[] arrY = new int[N];
+        
+        for (int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            arrX[i] = Integer.parseInt(st.nextToken());
+            arrY[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arrX);
+        Arrays.sort(arrY);
+        
+        System.out.println((arrX[N-1]-arrX[0]) * (arrY[N-1]-arrY[0]));
+        
+    }
+}
