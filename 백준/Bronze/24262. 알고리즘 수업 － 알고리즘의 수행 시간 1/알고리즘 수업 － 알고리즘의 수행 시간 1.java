f1b16e56f605ab1b0d5ddcfa4b93544bb5666e78
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-01-29 16:44:22 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 92 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(1); // 수행횟수: 항상 1
+        System.out.println(0); // 시간복잡도: O(1) (상수 시간)
+    }
+}
