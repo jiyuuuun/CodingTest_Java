2f18b524282abc9b033836e3ee51fbf9f350702a
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-01-31 19:45:32 +0900

    [Bronze II] Title: 수 정렬하기, Time: 120 ms, Memory: 14848 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,25 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int A[] = new int[N];
-        for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[N];
+        
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        // 버블 정렬을 구현하는 영역
-        for (int i = 0; i < N-1; i++) {
-            for (int j = 0; j < N-1-i; j++) {
-                if (A[j] > A[j+1]) {
-                    int temp = A[j];
-                    A[j] = A[j+1];
-                    A[j+1] = temp;
-                }
-            }
-        }
-        for (int i = 0; i < N; i++) {
-            System.out.println(A[i]);
+        
+        Arrays.sort(arr);
+        
+        for (int a : arr) {
+            System.out.println(a);
         }
     }
 }
