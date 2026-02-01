a0e6342f253a37ad482ccc2bcd5bcfaaebeaa900
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-02-01 16:46:11 +0900

    [Silver IV] Title: 문자열 집합, Time: 356 ms, Memory: 31184 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,24 +1,28 @@
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt(); // 집합 S에 포함된 문자열의 개수
-        int M = sc.nextInt(); // 검사해야 하는 문자열의 개수
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken()); // 집합에 포함된 수
+        int M = Integer.parseInt(st.nextToken()); // 검사해야 하는 수
+        int count = 0;
+        
         Set<String> set = new HashSet<>();
 
-        for (int i = 0; i < N; i++) {
-            String str = sc.next();
-            set.add(str);
+        for (int i=0; i<N; i++) {
+            set.add(br.readLine());
         }
-
-        int answer = 0;
-        for (int i = 0; i < M; i++) {
-            String str = sc.next();
-            if(set.contains(str)) answer++;
+        
+        for (int i=0; i<M; i++) {
+            String s = br.readLine();
+            
+            if (set.contains(s)) count++;
         }
-        System.out.print(answer);
+        
+        System.out.println(count);
     }
 }
