b931206b6d54d796e18230fa90145f78a9402a62
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-02-23 14:25:51 +0900

    [Silver IV] Title: 수 찾기, Time: 472 ms, Memory: 42960 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,30 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        // HashSet 사용
-        HashSet<Integer> set = new HashSet<>();
-
-        String[] input = br.readLine().split(" ");
-        for (int i = 0; i < N; i++) {
-            set.add(Integer.parseInt(input[i]));
+        
+        int[] A = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
-
+        
+        Arrays.sort(A);
+        
         int M = Integer.parseInt(br.readLine());
-
-        String[] con = br.readLine().split(" ");
-        for (int i = 0; i < M; i++) {
-            if (set.contains(Integer.parseInt(con[i]))) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
+        st = new StringTokenizer(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i=0; i<M; i++) {
+            int find = Integer.parseInt(st.nextToken());
+            
+            int left = 0;
+            int right = N-1;
+            boolean found = false;
+            
+            // 이분 탐색
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                
+                if (A[mid] == find) {
+                    found = true;
+                    break;
+                } else if (A[mid] < find){
+                    left = mid +1 ;
+                } else {
+                    right = mid - 1;
+                }
             }
+            
+            if (found) sb.append(1);
+            else sb.append(0);
+            
+            sb.append("\n");
         }
+        
+        System.out.println(sb);
     }
 }
