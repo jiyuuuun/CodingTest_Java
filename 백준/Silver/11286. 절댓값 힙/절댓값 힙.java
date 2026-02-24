f3fe654119d95962b2721e67955edeae1eb16557
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-02-24 15:21:34 +0900

    [Silver I] Title: 절댓값 힙, Time: 284 ms, Memory: 25576 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,33 +1,37 @@
+import java.util.*;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-
 
 public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> myQueue = new PriorityQueue<>((o1,o2)->{
-          int first_abs = Math.abs(o1);
-          int second_abs = Math.abs(o2);
-          if(first_abs == second_abs) { // 절댓값이 같은 경우 음수 우선
-              return o1 > o2 ? 1 : -1;
-          }
-          return first_abs - second_abs; // 절댓값 작은 데이터 우선
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> {
+            // 절댓값이 같으면
+            if (Math.abs(a) == Math.abs(b)) {
+                return a - b;
+            } else {
+                return Math.abs(a) - Math.abs(b);
+            }
         });
-
-        for (int i = 0; i < N; i++) {
-            int request = Integer.parseInt(br.readLine());
-            if(request == 0) {
-                if(myQueue.isEmpty()) {
-                    System.out.println("0");
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while (N-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+            
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
                 } else {
-                    System.out.println(myQueue.poll());
+                    sb.append(pq.poll()).append("\n");
                 }
             } else {
-                myQueue.add(request);
+                pq.offer(x);
             }
         }
+        
+        System.out.println(sb);
     }
 }
