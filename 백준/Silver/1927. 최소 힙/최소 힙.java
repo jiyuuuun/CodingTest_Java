c8d2c3a690869c963cdc5923a416a60d46c4f524
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-02-24 14:24:14 +0900

    [Silver II] Title: 최소 힙, Time: 276 ms, Memory: 26016 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,24 +1,31 @@
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-
-        PriorityQueue<Integer> heap = new PriorityQueue<>(); // 우선순위 큐
-
-        for (int i = 0; i < N; i++) {
-            int x = sc.nextInt();
-
-            if(x == 0) {
-                if(heap.isEmpty()) {
-                    System.out.println(0);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        // 우선순위 큐 (최소 힙 - 오름차순)
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while (N-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+            
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
                 } else {
-                    System.out.println(heap.poll());
+                    sb.append(pq.poll()).append("\n");
                 }
             } else {
-                heap.add(x);
+                pq.offer(x);
             }
         }
+        
+        System.out.println(sb);
     }
 }
