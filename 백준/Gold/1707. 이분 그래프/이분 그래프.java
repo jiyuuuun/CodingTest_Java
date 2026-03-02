ac536505d4d4fe3c218bbdb21daa8ee7bf77980c
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-03-02 17:52:22 +0900

    [Gold IV] Title: 이분 그래프, Time: 988 ms, Memory: 278932 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,58 +1,74 @@
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
-    static ArrayList<Integer>[] A;
-    static int[] check;
-    static boolean[] visited;
-    static boolean IsEven;
+    static ArrayList<Integer>[] graph;
+    static int[] color; // 0: 미방문, 1: 그룹A, -1:그룹B
+    static int V,E;
+    static boolean isBipartite;
     
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = Integer.parseInt(br.readLine());
-        for (int t = 0; t < testCase; t++) {
-            String[] s = br.readLine().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            A = new ArrayList[V+1];
-            visited = new boolean[V+1];
-            check = new int[V+1];
-            IsEven = true;
-            for (int i = 0; i <= V; i++) {
-                A[i] = new ArrayList<Integer>();
+        int K = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while (K-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken()); // 정점 개수
+            int E = Integer.parseInt(st.nextToken()); // 간선 개수
+            
+            graph = new ArrayList[V+1];
+            color = new int[V+1];
+            isBipartite = true;
+            
+            // 그래프 초기화
+            for (int i=1; i<=V; i++) {
+                graph[i] = new ArrayList<>();
             }
-            // 에지 데이터 저장하기
-            for (int i = 0; i < E; i++) {
-                s = br.readLine().split(" ");
-                int start = Integer.parseInt(s[0]);
-                int end = Integer.parseInt(s[1]);
-                A[start].add(end);
-                A[end].add(start);
+            
+            // 간선 입력
+            for (int i=0; i<E; i++) {
+                st = new StringTokenizer(br.readLine());
+                int u = Integer.parseInt(st.nextToken()); 
+                int v = Integer.parseInt(st.nextToken()); 
+                
+                graph[u].add(v); 
+                graph[v].add(u); // 무방향
             }
-            // 모든 노드에서 DFS 실헹
-            for (int i = 1; i <= V; i++) {
-                if(IsEven) {
-                    DFS(i);
-                } else {
-                    break;
+            
+            // 모든 정점 탐색 (그래프가 여러 덩어리일 수 있음)
+            for (int i=1; i<=V; i++) {
+                if (color[i] == 0) {
+                    bfs(i);
                 }
             }
-            if(IsEven) System.out.println("YES");
-            else System.out.println("NO");
+            
+            sb.append(isBipartite? "YES\n" : "NO\n");
         }
+        
+        System.out.println(sb);
     }
-
-    private static void DFS(int start) {
-        visited[start] = true;
-        for (int i : A[start]) {
-            if(!visited[i]) {
-                check[i] = (check[start] + 1) % 2;
-                DFS(i);
-            }
-            else if(check[start] == check[i]) {
-                    IsEven = false;
+    
+    static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        color[start] = 1; // 시작 정점은 1로 칠하기
+        
+        while (!q.isEmpty() && isBipartite) {
+            int cur = q.poll();
+            for (int next : graph[cur]) {
+                // 아직 색이 없다면 -> 반대 색으로 칠하기
+                if (color[next] == 0) {
+                    color[next] = -color[cur];
+                    q.add(next);
+                }
+                // 이미 색이 있는데 같은 색이면 -> 이분 그래프 아님
+                else if (color[next] == color[cur]) {
+                    isBipartite = false;
                     return;
+                }
             }
         }
     }
