5fdd2ca7fa916fd1c9f1cc24a82682fde62c6e91
Jun <132816893+jiyuuuun@users.noreply.github.com>
2026-03-10 14:23:26 +0900

    [Gold V] Title: 두 용액, Time: 328 ms, Memory: 30392 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,44 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(bf.readLine()); // 용액의 특성값
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 전체 용액 수
+        
         int[] A = new int[N];
-
-        String[] input = bf.readLine().split(" ");
-        for (int i = 0; i < N; i++) {
+        
+        String[] input = br.readLine().split(" ");
+        for (int i=0; i<N; i++) {
             A[i] = Integer.parseInt(input[i]);
         }
-
-        Arrays.sort(A);
-
+        
+        Arrays.sort(A); // 정렬
+        
         int left = 0;
         int right = N-1;
-        int ansLeft = A[left];
-        int ansRight = A[right];
-        long minSum = Math.abs((long)A[left] + A[right]);
-
+        
+        int min = Integer.MAX_VALUE;
+        int ans1 = 0;
+        int ans2 = 0;
+        
         while (left < right) {
-            long sum = (long)A[left] + A[right];
-
-            if(Math.abs(sum) < minSum) {
-                minSum = Math.abs(sum);
-                ansLeft = A[left];
-                ansRight = A[right];
+            int sum = A[left] + A[right];
+            
+            // 0에 더 가까운 값이면 갱신
+            if (Math.abs(sum) < min) {
+                min = Math.abs(sum);
+                ans1 = A[left];
+                ans2 = A[right];
             }
-
-            if(sum > 0) {
-                right--; // 합이 크니까 줄이기
+            
+            // 포인터 이동
+            if (sum > 0) {
+                right--;
             } else {
-                left++; // 합이 작으니까 키우기
+                left++;
             }
         }
-
-        System.out.print(ansLeft + " " + ansRight);
+        
+        System.out.println(ans1 + " " + ans2);
     }
 }
